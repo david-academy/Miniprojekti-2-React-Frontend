f378fa99e8c9d5a3783433baab6206b034bf775e
david-academy <david.andberg@gmail.com>
2019-08-01 09:16:22 +0300

    Update toimii, post ei

--- src/component/RecipeApp.jsx
+++ src/component/RecipeApp.jsx
@@ -1,17 +1,25 @@
 import React, { Component } from 'react';
 import ListRecipesComponent from "./ListRecipesComponent";
-import Accordion from "./Accordion";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import RecipeComponent from './RecipeComponent';
 
 class RecipeApp extends Component {
     render() {
-        return (<>
-        <h1>Recipe Application</h1>
-        <ListRecipesComponent/>
+        return (
+            <Router>
+                <>
+                    <h1>Recipe Application</h1>
+                    <Switch>
+                        <Route path="/" exact component={ListRecipesComponent} />
+                        <Route path="/reseptit" exact component={ListRecipesComponent} />
+                        <Route path="/reseptit/:id" component={RecipeComponent} />
+                        <Route path="/reseptit/uusi" component={ListRecipesComponent}/>
 
-
-            </>
+                        {/*<ListRecipesComponent/>*/}
+                    </Switch>
+                </>
+            </Router>
         )
     }
 }
-
 export default RecipeApp
